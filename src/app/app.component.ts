7ceb3ab17fc636a4e180912ddbcf19a6fb33edae
Mai Duong <113920406+hgmaidg@users.noreply.github.com>
2024-03-27 11:47:21 +0700

    base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: []
+  selector: "app-root",
+  template: ``,
+  styles: [],
 })
 export class AppComponent {
-  title = 'store';
+  title = "store";
 }
